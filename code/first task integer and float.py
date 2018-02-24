18bbf27134ac8aaa196e20cc45a50c3e90d33efe
666bury <buryj666@gmail.com>
2018-02-24 20:11:06 +0100

    second commit packt tutorials

---
+++ code/first task integer and float.py
@@ -0,0 +1,14 @@
+dane = "Hello world"
+print(dane)
+
+a = 3.5
+b = 6.73
+
+print(int(a))
+print(int(b))  #dodanie int powoduje przekształcenie liczb na całkowite
+
+
+c = 1
+
+print(float(c))  #dodanie float powoduje przekształcenie liczby na zmienno przecinkową 
+
---
+++ code/second task basic arithmetic operators.py
@@ -0,0 +1,8 @@
+print(2 + 4)
+print(3 - 5)
+print(2 - -5)
+print(2 * 4)
+print(2 ** 4)               #potęgowanie
+print(6 % 4)                #modulo
+print(6 / 4)                #zwykłe dzielenie
+print(6 // 4)               #dzielenie bez reszty (liczby całkowite)
