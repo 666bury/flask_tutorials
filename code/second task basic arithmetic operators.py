3467daf8233595ac1bb53d6db3cf6dbf75625f65
666bury <buryj666@gmail.com>
2018-02-24 20:20:14 +0100

    adnotacja modulo

--- code/second task basic arithmetic operators.py
+++ code/second task basic arithmetic operators.py
@@ -3,6 +3,6 @@ print(3 - 5)
 print(2 - -5)
 print(2 * 4)
 print(2 ** 4)               #potęgowanie
-print(6 % 4)                #modulo
+print(6 % 4)                #modulo reszta z dzielenia (wynikiem jest licznik reszty)
 print(6 / 4)                #zwykłe dzielenie
-print(6 // 4)               #dzielenie bez reszty (liczby całkowite)
+print(6 // 4)               #dzielenie w dół (liczby całkowite)
