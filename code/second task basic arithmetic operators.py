42b459fd614d44317d0eb18a79dc3ec78e9aea96
666bury <buryj666@gmail.com>
2019-02-11 13:46:28 +0100

    init clear  flask repo

--- code/second task basic arithmetic operators.py
+++
@@ -1,8 +0,0 @@
-print(2 + 4)
-print(3 - 5)
-print(2 - -5)
-print(2 * 4)
-print(2 ** 4)               #potęgowanie
-print(6 % 4)                #modulo reszta z dzielenia (wynikiem jest licznik reszty)
-print(6 / 4)                #zwykłe dzielenie
-print(6 // 4)               #dzielenie w dół (liczby całkowite)
